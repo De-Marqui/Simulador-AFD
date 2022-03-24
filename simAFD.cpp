005a1f84f1e9d05fbb9d688e93335988fd2ff242
Victor De Marqui <victor.demarqui@gmail.com>
2022-03-24 02:46:47 -0300

    Update simAFD.cpp

--- simAFD.cpp
+++ simAFD.cpp
@@ -1,160 +1,68 @@
-/*
-Guilherme Rodrigues Santos- 32012209
-Gustavo Hiroshi Yoshio - 32033273
-Victor de Marqui - 32014287
-*/
-
-#include <iostream>
-#include <stdio.h>
-#include <fstream>
+#include "afd.hpp"
 #include <string>
-#include <stdlib.h>
-#include <vector>
-#include <sstream>
-#include <cctype>
-#include <iostream>
-#include <cstring>
-#include <algorithm> 
-
 using namespace std;
 
-
-struct AFDConfig
+int main()
 {
-	int symbolQTD;
-	vector<string> symbols;
-	int stateNumber;
-	int finalStateQTD;
-	int finalState;
-	vector<string> transitions;
-};
+	int option = -1;
+	int controlFlag = 0;
+	string screenClearControl = "0";
+	string cadeia = "";
 
-int qtdLinhas;
+	while (option != 0) {
+		cout << "[Bem vindo, a seguir, temos algumas opções para escolher]" << endl;
+		cout << "---------------------------------------------------------" << endl;
+		cout << "| 1. Ler dados do AFD de arquivo" << endl
+			 << "| 2. Mostrar a quíntupla para o AFD lido" << endl
+			 << "| 3. Avaliar cadeias lidas de arquivo" << endl
+			 << "| 4. Avaliar novas cadeias pelo teclado" << endl
+			 << "| 0. Encerrar a aplicação" << endl << endl
+			 << endl << ">> Insira a opção: ";
 
-void AFDReading(){
-	AFDConfig AFD;
-  ifstream file;
-  string arqName;
-  string line;
-	bool flag = false;
-  int counter = 1;
-  int counts = 0;
-  
-  cout << "Digite o Nome do Arquivo AFDM Para Ser Lido: ";
-  cin >> arqName;
-	file.open(arqName);
+		cin >> option;
 
-  if(!file){
-    cout << "[ARQUIVO NAO PODE SER LIDO]\n" << endl;
-    cout << "[NOME DE ARQUIVO INVALIDO]\n" << endl;
-	}
-	
-	while(getline(file, line)){
-		switch(counter){
-      case 1:
-				for (int i = 0; i < line.size(); i++){
-					if(!isdigit(line[i])){
-						cout << "[ERRO - A QUANTIDADE DE CADEIAS NAO EH INTEIRO]\n" << endl;
-						return;
-					}
-				}	
-				AFD.symbolQTD  = stoi(line);
-				counter++;
-			  break;
-			
-      case 2:
-				stringstream ss(line);
-				string temp;
-				while (ss >> temp){
-					counts++;
-					if(islower(temp[0]) && (temp.size() == 1) && counts <= 12)
-						AFD.symbols.push_back(temp);  
-					else{
-						flag = true;
-						if(temp.size() > 1)
-							cout << "[ERROR -> CHAR.SIZE() > 1]" << endl;
-          	else if(counts > 12){
-							cout << "[ERROR -> SIZEOF() > 12]" << endl;
-							break;
-						}
-						else
-							cout << "[ERROR -> CHAR != LOWER]" << endl;
-					} 
+		switch (option) {
+		case 0:
+			exit(0);
+		case 1:
+			if(AFDReading()) 
+				controlFlag++;
+			break;
+
+		case 2:
+			if (controlFlag >= 1) 
+				printQuintuple();
+			break;
+
+		case 3:
+			if (controlFlag >= 1) 
+				fileReading();
+			break;
+
+		case 4:
+			if (controlFlag >= 1)
+			{
+				while (cadeia != "RETURN")
+				{
+					cout << endl << "Se deseja voltar, digite RETURN";
+					cout << endl << "Digite a cadeia que deseja validar: ";
+					cin >> cadeia;
+					if (cadeia != "RETURN")	
+						stringCheck(cadeia);
 				}
-			
-		    
-				if ((unique(AFD.symbols.begin(), AFD.symbols.end()) != AFD.symbols.end())) 
-					cout << "[ERROR -> REPITED CHAR]" << endl;
-				
-				if (flag) return;
-
-				counter++;
-        break;
-    }
-				
-	}
-  counter++;
-}
-
-
-void fileReading(){
-  ifstream file;
-  string fileName;
-  string line;
-  int counter = 1;
-
-  cout << "Digite o Nome do Arquivo Para ser lido: ";
-  cin >> fileName;
-  file.open(fileName);
-  
-  if(!file){
-    cout << "[ARQUIVO NAO PODE SER LIDO]\n" << endl;
-    cout << "[NOME DE ARQUIVO INVALIDO]\n" << endl;
-  }
-  
-  while(getline(file, line)){
-    if(counter == 1){
-      for (int i = 0; i < line.size(); i++){
-        if(!isdigit(line[i])){
-          cout << "[ERRO - A QUANTIDADE DE CADEIAS NAO EH INTEIRO]\n" << endl;
-          return;
-        }
-				qtdLinhas = stoi(line);
-      }		
-    }
-		if ( line.size() > 100){
-			cout << "[ERRO - A QUANTIDADE DE CARACTERES PRESENTES NA LINHA " << counter << " EH SUPERIOR A 100]\n" << endl;
-			return;
+				cadeia = "";
+			}
+			break;
 		}
-    counter++;
-  }
+		
+		if (controlFlag < 1) cout << "[ERROR -> afd.txt NOT READED CORRECTLY]" << endl;
 
-  cout << "[ARQUIVO LIDO COM SUCESSO]\n" << endl;
-}
+		do {
+			cout << endl << "[1. TO CONTINUE]" << endl << ">> ";
+			cin >> screenClearControl;
+		} while (screenClearControl != "1");
 
-int main() {
-  int option;
-
-  while(option!=0){
-    cout << "Escolha a opção digitando um número: " << endl;
-    cout << "1. Ler dados do AFD de arquivo." << endl;
-    cout << "2. Mostrar a quíntupla para o AFD lido." << endl;
-    cout << "3. Avaliar cadeias lidas de arquivo." << endl;
-    cout << "4. Avaliar novas cadeias pelo teclado." << endl;
-    cout << "0. Encerrar a sessão." << endl;
-    cout << "Resposta: ";
-    cin >> option;
-    cout << "\n";
-    switch(option){
-      case 1:
-        AFDReading();
-        break;
-      case 2:
-        break;
-      case 3:
-        break;
-      case 4:
-        break;
-    }
-  }
+		system("cls");
+		screenClearControl = "0";
+	}
 }
