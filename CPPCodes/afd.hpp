2bb2eb7ba50a89188eb8c458153fb5363fd061f3
Victor De Marqui <victor.demarqui@gmail.com>
2022-03-27 22:36:10 -0300

    Create afd.hpp

---
+++ CPPCodes/afd.hpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+
+using namespace std;
+
+struct transition
+{
+	int initState;
+	string symbols;
+	int nextState;
+	bool isFinalState;
+};
+
+struct AFDConfig
+{
+	int LinesQTD;
+	int symbolQTD;
+	vector<string> symbols;
+	int stateNumber;
+	int finalStateQTD;
+	vector<int> finalState;
+	int transitionsQTD;
+	struct transition transitionsStruct[30];
+};
+
+bool isDigitNumber(int*, string);
+bool AFDReading();
+void fileReading();
+bool AFDAccept(struct transition[]);
+void printQuintuple();
+void auxPrint(string, char, string, vector<string>);
+void stringCheck(string);
